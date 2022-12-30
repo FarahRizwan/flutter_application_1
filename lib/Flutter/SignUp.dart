c443ae31431bbc8193b93df2386069d9c4ca6cb3
Farah Rizwan <119890115+FarahRizwan@users.noreply.github.com>
2022-12-30 20:57:52 +0500

    changes

--- lib/Flutter/SignUp.dart
+++ lib/Flutter/SignUp.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Flutter/new.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -19,97 +20,117 @@ class _SignupState extends State<Signup> {
               image: AssetImage("images/beauty.jpeg"), fit: BoxFit.cover)),
       child: Scaffold(
           backgroundColor: Colors.transparent,
-          body: Column(
-            children: [
-              Text(
-                "SignUp",
-                style: TextStyle(fontSize: 30, color: Colors.black54),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 250, vertical: 20),
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: Color.fromARGB(255, 180, 100, 100), width: 2),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.miniStartFloat,
+          floatingActionButton: FloatingActionButton.small(
+            focusColor: Colors.purple,
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: ((context) => const application())));
+            },
+            child: Icon(
+              Icons.arrow_back,
+              color: Colors.brown,
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  "SignUp",
+                  style: TextStyle(fontSize: 30, color: Colors.black54),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 250, vertical: 20),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color.fromARGB(255, 180, 100, 100),
+                            width: 2),
+                      ),
+                      labelText: "First Name",
+                      prefixIcon: Icon(Icons.person),
+                      prefixIconColor: Colors.brown,
+                      fillColor: Colors.grey,
                     ),
-                    labelText: "First Name",
-                    prefixIcon: Icon(Icons.person),
-                    prefixIconColor: Colors.brown,
-                    fillColor: Colors.grey,
                   ),
                 ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 250, vertical: 20),
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: Color.fromARGB(255, 180, 100, 100), width: 2),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 250, vertical: 20),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color.fromARGB(255, 180, 100, 100),
+                            width: 2),
+                      ),
+                      labelText: "Last Name",
+                      prefixIcon: Icon(Icons.person),
+                      prefixIconColor: Colors.brown,
+                      fillColor: Colors.grey,
                     ),
-                    labelText: "Last Name",
-                    prefixIcon: Icon(Icons.person),
-                    prefixIconColor: Colors.brown,
-                    fillColor: Colors.grey,
                   ),
                 ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 250, vertical: 40),
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    filled: true,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: Color.fromARGB(255, 180, 100, 100), width: 2),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 250, vertical: 40),
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      filled: true,
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: const BorderSide(
+                            color: Color.fromARGB(255, 180, 100, 100),
+                            width: 2),
+                      ),
+                      labelText: "Email Address",
+                      prefixIcon: Icon(Icons.email),
+                      prefixIconColor: Colors.brown,
+                      fillColor: Colors.grey,
                     ),
-                    labelText: "Email Address",
-                    prefixIcon: Icon(Icons.email),
-                    prefixIconColor: Colors.brown,
-                    fillColor: Colors.grey,
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Radio(
-                      value: "pak",
-                      groupValue: pak,
-                      onChanged: ((value) {
-                        setState(() {
-                          pak = value.toString();
-                        });
-                      })),
-                  const Text("I agree to the terms and conditions")
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 60),
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(20)),
-                  height: 50,
-                  width: 150,
-                  child: const Center(
-                      child: Text(
-                    "SignUp",
-                    style: TextStyle(fontSize: 20),
-                  )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Radio(
+                        value: "pak",
+                        groupValue: pak,
+                        onChanged: ((value) {
+                          setState(() {
+                            pak = value.toString();
+                          });
+                        })),
+                    const Text("I agree to the terms and conditions")
+                  ],
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 60),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(20)),
+                    height: 50,
+                    width: 150,
+                    child: const Center(
+                        child: Text(
+                      "SignUp",
+                      style: TextStyle(fontSize: 20),
+                    )),
+                  ),
+                ),
+              ],
+            ),
           )),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'Flutter/Flutterlogo.dart';
+import 'Flutter/new.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false, home: logo());
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: application());
   }
 }
